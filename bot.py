0ef92f349d73f49a295f1cddab1956c0beb78af0
gaurav <gauravb8@github.com>
2020-03-25 16:47:23 +0530

    basic routing and twilio setup

--- bot.py
+++ bot.py
@@ -11,14 +11,18 @@ def bot():
 	resp = MessagingResponse()
 	msg = resp.message()
 	responded = False
-	if 'hi' in incoming_msg:
-		msg = 'hello'
-		msg.body(msg)
+	if 'hi' in incoming_msg.lower():
+		msg.body('hello')
 		responded = True
-	if 'hello' in incoming_msg:
-		msg = 'hey'
-		msg.body(msg)
+	if 'hello' in incoming_msg.lower():
+		msg.body('Wassup')
+		responded = True
+	if '?' in incoming_msg:
+		msg.body("Well I'm glad you asked. I am a chat bot developed to service your needs. Go on and enter your query.")
 		responded = True
 	if not responded:
 		msg.body('My training is under process.')
 	return str(resp)
+
+if __name__ == '__main__':
+	app.run()
