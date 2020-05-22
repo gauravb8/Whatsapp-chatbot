ed165ad3f674d1038f686a5f06bf332348b2bf5c
vivekjoshi731 <vivekjoshi731@gmail.com>
2020-05-22 13:43:49 +0530

    movie recommendation code added

--- app/utils.py
+++ app/utils.py
@@ -1,6 +1,10 @@
 from flask import current_app as app
 from wit import Wit
 import json
+import pandas as pd
+import numpy as np
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 
 
 
@@ -39,6 +43,48 @@ class Data(object):
 
 	def get_value(self, key):
 		return self.json_obj.get(key, None)
-		
+
+class Movies_Data(object):
+
+	def __init__(self,path):
+		self.df = pd.read_csv(path)
+		self.features = ['keywords','cast','genres','director']
+
+	def combine_features(self,row):
+		all_features = ''
+		for feature in self.features:
+			all_features += str(row[feature])
+		return all_features
+
+	def get_title_from_index(self,index):
+		return self.df[self.df.index == index]["title"].values[0]
+
+	def get_popularityy_from_index(self,index):
+		return self.df[self.df.index == index]["popularity"].values[0]
+
+	def get_index_from_title(self,title):
+		return self.df[self.df.title == title]["index"].values[0]
+
+
+	def find_similar_movies(self,movie_user_likes):
+		top_movies = {}
+		for feature in self.features:
+			self.df[feature] = self.df[feature].fillna('')
+
+		tqdm.pandas()
+		self.df["combined_features"] = self.df.progress_apply(self.combine_features,axis=1)
+		cv = CountVectorizer()
+		count_matrix = cv.fit_transform(self.df["combined_features"])
+		cosine_sim = cosine_similarity(count_matrix)
+		movie_index = self.get_index_from_title(movie_user_likes)
+		similar_movies =  list(enumerate(cosine_sim[movie_index]))
+		sorted_similar_movies = sorted(similar_movies,key=lambda x:x[1],reverse=True)[1:]
+		top_movies = {self.get_title_from_index(sorted_similar_movies[i][0]):self.get_popularityy_from_index(sorted_similar_movies[i][0]) for i in range(10)}
+		return  [name[0] for name in sorted(top_movies.items(), key=lambda x: x[1],reverse= True)]
+
+
+
+
+
 		
 
