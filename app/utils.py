9402909e3b7f8e81b6f218f65596bc8e3cd43f5f
vivekjoshi731 <vivekjoshi731@gmail.com>
2020-05-24 12:58:39 +0530

    dataset updated

--- app/utils.py
+++ app/utils.py
@@ -5,7 +5,7 @@ import pandas as pd
 import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
-
+from tqdm import tqdm
 
 
 class WitInterface(object):
